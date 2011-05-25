b9d0b6e98fb7e45a698d095b7431a039ef0afa0c
bevenky <venky@miglu.com>
2011-05-25 17:44:18 +0530

    Change URL to Url

--- examples/example-transfercall.rb
+++ examples/example-transfercall.rb
@@ -15,7 +15,7 @@ plivo = Plivo::Rest.new(REST_API_URL, SID, AUTH_TOKEN)
 
 # Transfer a call using a HTTP POST
 transfer_call_params = {
-    'URL' => "http://127.0.0.1:5000/transfered/",
+    'Url' => "http://127.0.0.1:5000/transfered/",
     'CallUUID' => 'edaa59e1-79e0-41de-b016-f7a7570f6e9c', # Request UUID to hangup call
 }
 
